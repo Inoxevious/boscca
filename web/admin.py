fbca54e80d4be6f33627fde90c47d7253ec90288
Inoxevious <mpasiinnocent@gmail.com>
2021-03-08 15:19:13 +0200

    update

--- web/admin.py
+++ web/admin.py
@@ -3,50 +3,8 @@ from django.contrib.contenttypes.admin import GenericTabularInline
 from web.models import *
 from sacco.models import *
 
-@admin.register(HomePage)
-class HomePageAdmin(admin.ModelAdmin):
-    inlines = (
-        HomePageSliderLineInline,HomePageFactsLineInline,HomePageObjectiveLineInline, HomePageArticleLineInline, HomePageTestimonialLineInline
-    )
 
 
-@admin.register(BoardMembers)
-class BoardMembersAdmin(admin.ModelAdmin):
-    pass
 
-@admin.register(Facts)
-class FactsAdmin(admin.ModelAdmin):
-    pass
 
-@admin.register(Testimonial)
-class TestimonialAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(HomePageObjective)
-class TestimonialAdmin(admin.ModelAdmin):
-    pass
-
-class HomePageSliderLineInline(admin.TabularInline):
-    model = HomePageSlider
-    extra = 1
-
-class HomePageFactsLineInline(admin.TabularInline):
-    model = HomePageFacts
-    extra = 1
-
-class HomePageTestimonialLineInline(admin.TabularInline):
-    model = HomePageTestimonial
-    extra = 1
-
-class HomePageArticleLineInline(admin.TabularInline):
-    model = HomePageArticle
-    extra = 1
-
-class HomePageObjectiveLineInline(admin.TabularInline):
-    model = HomePageObjective
-    extra = 1
-
-class NationalStaffMemberLineInline(admin.TabularInline):
-    model = NationalStaffMember
-    extra = 1
 

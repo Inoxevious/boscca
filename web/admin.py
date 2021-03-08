dc97554ad033a5d27cfd44de2123df28dc72695b
Inoxevious <mpasiinnocent@gmail.com>
2021-03-08 14:20:55 +0200

    update

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
+from web.models import *
+from sacco.models import *
+
+
+
+class HomePageSliderLineInline(admin.TabularInline):
+    model = HomePageSlider
+    extra = 1
+
+class HomePageFactsLineInline(admin.TabularInline):
+    model = HomePageFacts
+    extra = 1
+
+class HomePageTestimonialLineInline(admin.TabularInline):
+    model = HomePageTestimonial
+    extra = 1
+
+class HomePageArticleLineInline(admin.TabularInline):
+    model = HomePageArticle
+    extra = 1
+
+class HomePageObjectiveLineInline(admin.TabularInline):
+    model = HomePageObjective
+    extra = 1
+
+class NationalStaffMemberLineInline(admin.TabularInline):
+    model = NationalStaffMember
+    extra = 1
+
+@admin.register(HomePage)
+class HomePageAdmin(admin.ModelAdmin):
+    inlines = (
+        HomePageSliderLineInline,HomePageFactsLineInline,HomePageObjectiveLineInline, HomePageArticleLineInline, HomePageTestimonialLineInline
+    )
+
+
+@admin.register(BoardMembers)
+class HomePageAdmin(admin.ModelAdmin):
+    pass
+
 
 # Register your models here.
--- web/models.py
+++ web/models.py
@@ -1,58 +1,67 @@
 from django.db import models
-from sacco.models import Article, Objective
+from sacco.models import Article, Objective, NationalStaffMember
 from users.models import AccountUser
 # Create your models here.
 class HomePage(models.Model):
-    logo = models.ImageField(null=True ,blank=True, upload_to='media/images')
-    mission =models.TextField(null=True ,blank=True)
-    def __str__(self):
-        return self.mission
+	logo = models.ImageField(null=True ,blank=True, upload_to='media/images')
+	mission =models.TextField(null=True ,blank=True)
+	def __str__(self):
+		return self.mission
 class HomePageSlider(models.Model):
-    image = models.ImageField(null=True ,blank=True, upload_to='media/images')
-    title =models.TextField(null=True ,blank=True)
-    sub_title1 =models.TextField(null=True ,blank=True)
-    sub_title2 =models.TextField(null=True ,blank=True)
-    def __str__(self):
-        return self.title
+	home_page = models.ForeignKey(HomePage, on_delete = models.CASCADE,null=True ,blank=True)
+
+	image = models.ImageField(null=True ,blank=True, upload_to='media/images')
+	title =models.TextField(null=True ,blank=True)
+	sub_title1 =models.TextField(null=True ,blank=True)
+	sub_title2 =models.TextField(null=True ,blank=True)
+	def __str__(self):
+		return self.title
 
 class Facts(models.Model):
-    video_url = models.TextField(null=True ,blank=True)
-    image = models.ImageField(null=True ,blank=True, upload_to='media/images')
-    title =models.TextField(null=True ,blank=True)
-    text =models.TextField(null=True ,blank=True)
-    def __str__(self):
-        return self.title
+	video_url = models.TextField(null=True ,blank=True)
+	image = models.ImageField(null=True ,blank=True, upload_to='media/images')
+	title =models.TextField(null=True ,blank=True)
+	text =models.TextField(null=True ,blank=True)
+	def __str__(self):
+		return self.title
 
 class Testimonial(models.Model):
-    profile = models.ForeignKey(AccountUser, on_delete = models.CASCADE,null=True ,blank=True)
-    video_url = models.TextField(null=True ,blank=True)
-    image = models.ImageField(null=True ,blank=True, upload_to='media/images')
-    title =models.TextField(null=True ,blank=True)
-    text =models.TextField(null=True ,blank=True)
-    def __str__(self):
-        return self.title
+	profile = models.ForeignKey(AccountUser, on_delete = models.CASCADE,null=True ,blank=True)
+	video_url = models.TextField(null=True ,blank=True)
+	image = models.ImageField(null=True ,blank=True, upload_to='media/images')
+	title =models.TextField(null=True ,blank=True)
+	text =models.TextField(null=True ,blank=True)
+	def __str__(self):
+		return self.title
 class HomePageFacts(models.Model):
-    facts = models.ForeignKey(Facts, on_delete = models.CASCADE, null=True ,blank=True)
-    def __str__(self):
-        return self.facts.title
+	home_page = models.ForeignKey(HomePage, on_delete = models.CASCADE,null=True ,blank=True)
+	facts = models.ForeignKey(Facts, on_delete = models.CASCADE, null=True ,blank=True)
+	def __str__(self):
+		return self.facts.title
 class HomePageTestimonial(models.Model):
-    testimonial = models.ForeignKey(Testimonial, on_delete = models.CASCADE, null=True ,blank=True)
-    def __str__(self):
-        return self.testimonial.title
-class HomePageArticle(models.Model):
-    article = models.ForeignKey(Article, on_delete = models.CASCADE, null=True ,blank=True)
+	home_page = models.ForeignKey(HomePage, on_delete = models.CASCADE,null=True ,blank=True)
 
-    def __str__(self):
-        return self.article.title
+	testimonial = models.ForeignKey(Testimonial, on_delete = models.CASCADE, null=True ,blank=True)
+	def __str__(self):
+		return self.testimonial.title
+class HomePageArticle(models.Model):
+	home_page = models.ForeignKey(HomePage, on_delete = models.CASCADE,null=True ,blank=True)
+
+	article = models.ForeignKey(Article, on_delete = models.CASCADE, null=True ,blank=True)
+
+	def __str__(self):
+		return self.article.title
 
 class HomePageObjective(models.Model):
-    video_url = models.TextField(null=True ,blank=True)
-    objective = models.ManyToManyField(Objective)
-    video_title =models.TextField(null=True ,blank=True)
-    video_subtitle =models.TextField(null=True ,blank=True)
+	home_page = models.ForeignKey(HomePage, on_delete = models.CASCADE,null=True ,blank=True)
 
-    def __str__(self):
-        return self.objective.title
+	video_url = models.TextField(null=True ,blank=True)
+	objective = models.ManyToManyField(Objective)
+	video_title =models.TextField(null=True ,blank=True)
+	video_subtitle =models.TextField(null=True ,blank=True)
+
+	def __str__(self):
+		return self.objective.title
 # class AboutUs(models.Model):
 #     logo = models.ImageField(null=True ,blank=True, upload_to='media/images')
 #     mission =models.TextField(null=True ,blank=True)
@@ -71,8 +80,9 @@ class HomePageObjective(models.Model):
 #         return self.user.first_name
 
 class BoardMembers(models.Model):
-    board_statement =models.TextField(null=True ,blank=True)
-    # mission =models.TextField(null=True ,blank=True)
-    def __str__(self):
-        return self.user.first_name
+	staff = models.ForeignKey(NationalStaffMember, on_delete = models.CASCADE,null=True ,blank=True)
+	board_statement =models.TextField(null=True ,blank=True)
+	# mission =models.TextField(null=True ,blank=True)
+	def __str__(self):
+		return self.user.first_name
 
